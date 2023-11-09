70f08c5d6be2a7292a706e6e71eb635adaed13dd
aziza <azizabonusaliyeva@gmail.com>
2023-11-09 11:40:15 +0500

    js-28
    js-28

---
+++ index.js
@@ -0,0 +1,32 @@
+let i = 0
+
+while (i <=9 ) {
+    i++
+    document.write(`7 * ${i} = ${i * 7} <br>`);
+}
+
+
+
+let a = 1
+
+do {
+    
+    document.write(` 8 * ${a} = ${a * 8} <br>`);
+
+    a++
+
+} while (a <= 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
